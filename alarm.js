3114f4b59c80db9752f5dc4fb124740313276dc1
Yeshwant-22 <yeshwant.rajaraman@cdw.com>
2025-02-20 12:02:17 +0530

    Update alarm.js

--- alarm.js
+++ alarm.js
@@ -148,5 +148,12 @@ async function getAlarmsData(selectedAccount) {
 }
 
 function toggleDarkMode() {
-  document.getElementsByTagName("body")[0].classList.toggle("dark-mode");
+            document.body.classList.toggle("dark-mode");
+            const toggleBtn = document.querySelector(".toggle-btn2");
+
+            if (document.body.classList.contains("dark-mode")) {
+                toggleBtn.innerHTML = "☀️"; // Switch to sun
+            } else {
+                toggleBtn.innerHTML = "🌙"; // Switch to moon
+            }
 }
