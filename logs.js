be34861d58882737dbf680443b42821d575c785b
Yeshwant-22 <yeshwant.rajaraman@cdw.com>
2025-02-20 12:02:54 +0530

    Update logs.js

--- logs.js
+++ logs.js
@@ -85,3 +85,13 @@ async function getAlarmsData(){
     }
 
 }
+function toggleDarkMode() {
+            document.body.classList.toggle("dark-mode");
+            const toggleBtn = document.querySelector(".toggle-btn2");
+
+            if (document.body.classList.contains("dark-mode")) {
+                toggleBtn.innerHTML = "☀️"; // Switch to sun
+            } else {
+                toggleBtn.innerHTML = "🌙"; // Switch to moon
+            }
+}
